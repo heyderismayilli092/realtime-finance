528a03cf0822aa76ff2cfc9b44bfd0ff1fb964f8
Heydər İsmayıllı <heyderismayilli092@gmail.com>
2026-05-18 18:51:46 +0400

    The location of the main code file was changed, and a loop function was prepared to run when the program opens (a 'test' counter was added to verify its functionality)

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, GObject
+from gi.repository import Gtk, Gdk, GObject, GLib
 
 import os
 import socket
@@ -14,44 +14,50 @@ from locale import gettext as _
 locale.bindtextdomain('pardus-finance', '/usr/share/locale')
 locale.textdomain('pardus-finance')
 
-GLADE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ui/MainWindow.glade")
+GLADE_FILE = os.path.dirname(os.path.abspath(__file__)) + "/../ui/MainWindow.glade"
 
 
 class pardusfinance:
     def __init__(self):
-        self.flask_process = None
-
         self.builder = Gtk.Builder()
         self.builder.add_from_file(GLADE_FILE)
 
         # widget referances
         self.window = self.builder.get_object("mainwindow")  # home window
-        self.window.set_decorated(False)  # remove window decoration
-
+        self.update_time_label = self.builder.get_object("update_time")
+        self.close_button = self.builder.get_object("closebutton")  # close button
 
         # transparency window
+        self.window.set_decorated(False)  # remove window decoration
         screen = self.window.get_screen()
         visual = screen.get_rgba_visual()
         if visual and screen.is_composited():
             self.window.set_visual(visual)
-
-
-        self.cssload()  # CSS desing
-
         self.window.set_keep_above(True)  # always keep it on top
 
         # signals
         self.window.connect("realize", self.on_realize)  # when realized, the Gdk.Window file belonging to the window will be ready
-        self.window.connect("destroy", self._on_destroy)
+        self.window.connect("destroy", self._quit)
+        self.close_button.connect("clicked", self._quit)
+        self.count = 0
+        GLib.timeout_add_seconds(2, self._tick)  # '_tick' fonksiyonu her defasında 3 saniyeden bir çalışır
         self.window.show_all()
+        self.cssload()  # CSS desing
 
 
     # CSS theme
     def cssload(self):
         css = b"""
-
-        window { background-color: rgba(80, 83, 84, 0.5); border-radius: 20px; }
-
+        window { background-color: rgba(80, 83, 84, 0.4); border-radius: 20px; color: #fff; }
+        button {
+            background-image: none;
+            background-color: #d64a4a;
+            color: #fff;
+            border-radius: 10px;
+        }
+        button:hover {
+          background-color: #560000;
+        }
         """
         provider = Gtk.CssProvider()
         provider.load_from_data(css)
@@ -75,7 +81,14 @@ class pardusfinance:
         y = geom.y + geom.height - height - 10
         win.move(x, y)
 
-    def _on_destroy(self, widget):
+    # finance API function
+    def _tick(self):
+        self.count += 1
+        self.update_time_label.set_text(str(self.count))
+        return True
+
+    # quit
+    def _quit(self, widget):
         Gtk.main_quit()
 
 
